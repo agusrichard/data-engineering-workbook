60dfe98e385606d582cbf7c9cbd4ac98d47de106
Agus Richard <agus.richard21@gmail.com>
2024-05-24 21:41:49 +0700

    another concept of concurrency in Python is acquired

---
+++ pokeapi-mongo-s3-airflow-etl/playground/async2.py
@@ -0,0 +1,49 @@
+import asyncio
+from asyncio import Queue
+from random import random
+
+
+LIMIT = 10000
+OFFSET = 50
+
+
+class StopException(Exception):
+    pass
+
+
+async def producer(queue: Queue):
+    print("Producer: Running")
+    i = 0
+    while True:
+        await queue.put(i)
+        i += OFFSET
+
+
+async def consumer(queue: Queue, num: int):
+    print(f"Consumer {num}: Running")
+    while True:
+        item = await queue.get()
+        await asyncio.sleep(random())
+        # try:
+        #     item = queue.get_nowait()
+        #     await asyncio.sleep(random())
+        # except asyncio.QueueEmpty:
+        #     print("Consumer: got nothing, waiting a while...")
+        #     await asyncio.sleep(0.5)
+        #     continue
+        if item > LIMIT:
+            raise StopException
+        print(f">got {item}")
+
+
+async def run():
+    try:
+        queue = Queue(100)
+        await asyncio.gather(producer(queue), *[consumer(queue, i) for i in range(10)])
+    except StopException:
+        print("Producer: Done")
+        print("All Consumers: Done")
+
+
+if __name__ == "__main__":
+    asyncio.run(run())
