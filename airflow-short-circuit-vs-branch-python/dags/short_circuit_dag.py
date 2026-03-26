ae8af356bb10f492d0fb9dc4225c230ebc403f9d
Agus Richard <agus.richard21@gmail.com>
2026-03-26 18:28:53 +0700

    start working on new article

---
+++ airflow-short-circuit-vs-branch-python/dags/short_circuit_dag.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from airflow.sdk import DAG, task
+
+
+with DAG(
+    dag_id="short_circuit_dag",
+    default_args={"owner": "airflow"},
+    schedule="@daily",
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+) as dag:
+    @task
+    def hello_world():
+        print("Hello World!")
+
+    hello_world()
